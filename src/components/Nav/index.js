1d097547f63a76b75b0ef64c05812fc1adf8aa36
Achu Ajesh <achuv37@gmail.com>
2023-01-19 20:36:18 -0500

    initial-setup of react-app

---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav () {
+    return (
+      <div>
+        <h2>Am here!</h2>
+      </div>
+    )
+}
+
+
+export default Nav;
---
+++ src/components/Thought/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Thought() {
+  return (
+    <section className="my-5">
+      <h1>My thoughts </h1>
+    
+    </section>
+  );
+}
+
+export default Thought;
